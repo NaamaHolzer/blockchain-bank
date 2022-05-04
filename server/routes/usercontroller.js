6fd78fae1355105da8e40741ee2e5fb7aa69883e
naamahol <naamaholzer1@gmail.com>
2022-05-04 12:04:34 +0300

    Fixed gitgnore

--- server/routes/usercontroller.js
+++ server/routes/usercontroller.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const checksession = require('../middlewares/checksession');
+const dotenv = require('dotenv');
+
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        console.log(err)
+
+        if (err) return res.sendStatus(403)
+
+        req.user = user
+
+        next()
+    })
+}
+
 
 router.post('/user/login',
     (req, res) => {
