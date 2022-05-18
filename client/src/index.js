6d743e096d51eea28838bed14ad6a97f99656632
naamahol <naamaholzer1@gmail.com>
2022-05-18 17:30:09 +0300

    Index files

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './components/Home/Home'
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#07877D'
+    },
+    secondary: {
+      main: '#07877D'
+    }
+  }
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+    <MuiThemeProvider theme={theme}>
+      <React.StrictMode>
+        <Home />
+      </React.StrictMode>
+    </MuiThemeProvider>
+  
+  );
+  
