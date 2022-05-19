f7b0235f76f9ec43ff56b0fc30e2b216b8908f42
naamahol <naamaholzer1@gmail.com>
2022-05-19 17:15:14 +0300

    Set default font for the app

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import Home from './components/Home/Home'
 import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 
 const theme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat, sans-serif'
+  },
   palette: {
     primary: {
       main: '#07877D'
